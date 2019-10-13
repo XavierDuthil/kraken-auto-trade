63f7ebe5cecc2fa254e3fd8dfaed4dfcfed4a543
Xavier Duthil <x.duthil.dev@gmail.com>
2019-10-13 23:13:13 +0200

    utils: get_colored_value now includes the "+" sign for positive numbers

--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ import coloredlogs
 
 
 def get_colored_value(value, suffix=''):
-    value_str = f'{+value:.02f}{suffix}'
+    value_str = f'{value:+.02f}{suffix}'
     if 0 < abs(value) < 1:
-        value_str = f'{+value:.04f}{suffix}'
+        value_str = f'{value:+.04f}{suffix}'
 
     if value > 0:
         return coloredlogs.ansi_wrap(value_str, color='green')
