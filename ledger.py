e2363b9639ddc1b73d87209fc57c618f8f71cd0b
Xavier Duthil <xduthil@qosenergy.com>
2020-02-11 14:26:45 +0100

    ledger: Implement a basic wallet to prevent an operation if funds are insufficient

--- ledger.py
+++ ledger.py
@@ -9,6 +9,8 @@ Buy, Sell = range(0, 2)
 logger = logging.getLogger('ledger')
 logger.addHandler(logging.FileHandler('log/ledger.log', 'w'))
 
+wallet_euros = 200
+
 
 class Operation:
     def __init__(self, pair, amount, price, kind):
@@ -29,15 +31,48 @@ class Ledger:
     def __init__(self, pair):
         self.operations = []
         self.pair = pair
-        self.initial_fiat_amount = 0
-        self.initial_crypto_amount = 0
+        self.current_amount = 0
+
+        # Amounts to ~25€ for each currency
+        if pair == "XXBTZEUR":
+            self.current_amount = 0.0028
+        elif pair == "XETHZEUR":
+            self.current_amount = 0.12
+        elif pair == "XXMRZEUR":
+            self.current_amount = 0.325
+        elif pair == "XLTCZEUR":
+            self.current_amount = 0.37
+        elif pair == "XXRPZEUR":
+            self.current_amount = 100
+        elif pair == "DASHEUR":
+            self.current_amount = 0.22
+        elif pair == "BCHEUR":
+            self.current_amount = 0.06
 
     def add_buy(self, amount, price):
+        global wallet_euros
         new_operation = Operation(self.pair, amount, price, Buy)
+
+        if wallet_euros - new_operation.real_price < 0:
+            logger.warning(f"Can't buy {new_operation.amount:.4f} {self.pair}: not enough euros ({wallet_euros:.2f}€)")
+            return
+
+        wallet_euros -= new_operation.real_price
+        self.current_amount += new_operation.amount
+        logger.info(f"{wallet_euros:.2f}€ in wallet")
         self.operations.append(new_operation)
 
     def add_sell(self, amount, price):
+        global wallet_euros
         new_operation = Operation(self.pair, amount, price, Sell)
+
+        if self.current_amount - new_operation.amount < 0:
+            logger.warning(f"Can't sell {new_operation.amount} {self.pair}: not enough tokens ({self.current_amount:.4f} {self.pair})")
+            return
+
+        wallet_euros += new_operation.real_price
+        self.current_amount -= new_operation.amount
+        logger.info(f"{wallet_euros:.2f}€ in wallet")
         self.operations.append(new_operation)
 
     def calculate_return(self, last_known_price):
